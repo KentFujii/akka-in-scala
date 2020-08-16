22c4d91d16d8b8330618891c95fa9deec9a3cbbc
KentFujii <kent.where.the.light.is@gmail.com>
2020-08-16 22:56:24 +0900

    wip: up-and-running

--- up-and-running/src/main/scala/Main.scala
+++ up-and-running/src/main/scala/Main.scala
@@ -1,8 +1,19 @@
 package up_and_running
 
 import com.typesafe.config.{Config, ConfigFactory}
+import akka.util.Timeout
 
-object Main extends App {
+object Main extends App with RequestTimeout {
   val config = ConfigFactory.load()
-  println(config)
+  println(config.getString("http.host"))
+  println(config.getInt("http.port"))
+}
+
+trait RequestTimeout {
+  import scala.concurrent.duration._
+  def requestTimeout(config: Config): Timeout = {
+    val t = config.getString("akka.http.server.request-timeout")
+    val d = Duration(t)
+    FiniteDuration(d.length, d.unit)
+  }
 }
